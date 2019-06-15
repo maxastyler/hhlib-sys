70b66a4514326b3f43c1ea0ab85f8a3f4e4244b2
Max Tyler <maxastyler@gmail.com>
2019-06-15 13:38:33 +0100

    Add types file to wrap the types

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[macro_use]
+extern crate num_derive;
+
+mod bindings {include!(concat!(env!("OUT_DIR"), "/bindings.rs"));};
+
+mod types;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::bindings::*;
     #[test]
     fn it_works() {
         let mut lib_array = [0i8, 0, 0, 0];
         unsafe {HH_GetLibraryVersion(lib_array.as_mut_ptr());}
+        assert_eq!(lib_array, [51i8, 46, 48, 0]);
     }
 }
