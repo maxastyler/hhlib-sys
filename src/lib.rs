2d2a63fefb156703f405f22236f722b5919f3496
Max Tyler <maxastyler@gmail.com>
2019-06-15 14:01:06 +0100

    Add more modes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate num_derive;
 
-mod bindings {include!(concat!(env!("OUT_DIR"), "/bindings.rs"));};
+mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
 
 mod types;
 
@@ -11,7 +13,9 @@ mod tests {
     #[test]
     fn it_works() {
         let mut lib_array = [0i8, 0, 0, 0];
-        unsafe {HH_GetLibraryVersion(lib_array.as_mut_ptr());}
+        unsafe {
+            HH_GetLibraryVersion(lib_array.as_mut_ptr());
+        }
         assert_eq!(lib_array, [51i8, 46, 48, 0]);
     }
 }
